0c5ab7d867c22351bd46c721b241924a9fcec83f
navitas28 <anshulsaxena2807@gmail.com>
2020-06-19 21:56:10 +0530

    json

--- src/components/UI/List/List.js
+++ src/components/UI/List/List.js
@@ -2,16 +2,14 @@ import React from 'react';
 import classes from './List.module.scss';
 import {faTrash} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { FacebookProvider, Share } from 'react-facebook';
+import {FacebookProvider, Share} from 'react-facebook';
 
 import ListItem from '../ListItem/ListItem';
-import Feed from '../../Feed/feed'
+import Feed from '../../Feed/feed';
 
 const list = (props) => (
-
   <div className={classes.List}>
     <div className={classes.ListHeader}>
-
       <span>{props.appName}</span>
       <a
         href={`https://play.google.com/store/apps/details?id=${props.packageName}`}
@@ -21,16 +19,12 @@ const list = (props) => (
       >
         <FontAwesomeIcon className={classes.bin} icon={faTrash} />
       </a>
-
     </div>
 
     <ul className={classes.UnorderedList}>
       <ListItem listItems={props.data} />
-
     </ul>
-
   </div>
-
 );
 
 export default list;
